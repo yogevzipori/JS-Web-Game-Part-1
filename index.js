a0b0883778aa25faa6526cb54d719943017b846b
Yogev Zipori <yogevzipori@Yogevs-MacBook-Pro.local>
2022-01-20 15:25:01 -0800

    First try

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage(imgFile, left, bottom) {
+let gameObject = document.createElement('img');
+gameObject.src = imgFile;
+gameObject.style.position = 'fixed';
+gameObject.style.left =left;
+gameObject.style.bottom = bottom;
+document.body.append(gameObject);
+return gameObject;
+
+}
+
+
+function newItem(imgFile, left, bottom) {
+    let gameObject = newImage(imgFile, left, bottom);
+    
+    gameObject.addEventListener('dblclick', () => {
+        gameObject.remove()
+    });
+    
+}
+
+
+
+newImage ("assets/green-character.gif", "100px" , "100px");
+newImage ("assets/pine-tree.png" , "450px", "200px");
+newImage ("assets/tree.png" , "200px" , "300px"); 
+newImage ("assets/pillar.png" , "350px" , "100px");
+newImage ("assets/crate.png" , "150px" , "200px");
+newImage ("assets/well.png" , "500px" , "425px");
+
+newItem("assets/sword.png" , "500px" , "405px");
+newItem("assets/sheild.png", "165px", "185px" );
+newItem("assets/staff.png" , "600px" , "100px");
+
+
+
+  
+
+
+
